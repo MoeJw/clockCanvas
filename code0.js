5bb699d232997d100202d3a5321ba1b3ccfb0508
Mohammd Omar <mohammdomar19@gmail.com>
2019-01-18 22:19:25 +0200

    make the pointer of clock much like clock pointer

--- code0.js
+++ code0.js
@@ -1,11 +1,10 @@
-node =new Node(130,150,50,50)
-node2 =new Node(200,200,70,250)
-one =new line(200,0,300,200)
+node2 =new Node(200,200,70,6) // the circle
+one =new line(200,200,250,200)
 //var x=[130,144,144,158,200,200,242,256,256,270]
 //var y=[150,108,192,94,206,80,220,94,206,108,192,150]
 var count=0
 angle=18
-function eq(cx=200,cy=200,r=70){
+function eq(cx=200,cy=200,r=60){
     if(false){
        // angle=0
        
