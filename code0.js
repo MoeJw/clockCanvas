8b6dc689b687ee13a61f371e1e287d65bc3fde8d
Mohammd Omar <mohammdomar19@gmail.com>
2019-01-19 09:53:42 +0200

    make the secondes pointer  look like the the clock pointer ..there still some buges needed to be fixed

--- code0.js
+++ code0.js
@@ -4,27 +4,59 @@ one =new line(200,200,250,200)
 //var y=[150,108,192,94,206,80,220,94,206,108,192,150]
 var count=0
 angle=18
+flag=1
+var startTime, endTime;
+
+function start() {
+  startTime = new Date();
+};
+
+function end() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime; //in ms
+  // strip the ms
+ timeDiff /= 1000;
+
+  // get seconds 
+  var seconds = Math.round(timeDiff);
+  console.log(seconds + " seconds");
+return seconds
+}
+
 function eq(cx=200,cy=200,r=60){
     if(false){
        // angle=0
        
     }else{
-        angle=angle+0.008
+        angle=angle+0.0025  
         requestAnimationFrame(Frame)
        
     }
-    x = cx + r * Math.cos(angle)
-    y = cy + r * Math.sin(angle)
    
-    one.Sx=x
-    one.Sy=y
-    one.ex=200
-    one.ey=200
+   
+     if(end()==2){
+        x = cx + r * Math.cos(angle)
+        y = cy + r * Math.sin(angle)
+        one.Sx=x
+        one.Sy=y
+        one.ex=200
+        one.ey=200 
+        flag=1
+         
+     }
+         
+        
+   
     console.log(angle)
 }
 
-function Frame(){
 
+function Frame(){
+    if(flag){
+        start()
+        flag=0
+    }
+         
     eq()
                       
     text.clearRect(0,0,window.innerWidth,window.innerHeight);
@@ -35,6 +67,7 @@ function Frame(){
     
        
 }
+
 
 
 
