a0243029e2a165d573d1e54c3e1d6a14adbf8ee4
Mohammd Omar <mohammdomar19@gmail.com>
2019-01-24 19:41:16 +0200

    make the Hour pointer more accurate and draw the  numbers inside the clock

--- code0.js
+++ code0.js
@@ -1,12 +1,112 @@
-node2 =new Node(200,200,70,6) // the circle
-one =new line(200,200,250,200)
+
 //var x=[130,144,144,158,200,200,242,256,256,270]
 //var y=[150,108,192,94,206,80,220,94,206,108,192,150]
+/*  so one radian is eq to 57.2925 right,
+    we need to make a function to convert the Minute to  the Hour range in Clock
+    ....     
+    2-->30
+    1   60
+    12   90
+    11   120
+    10   150
+    9    180
+    8    210
+    7    240
+    6    270
+    5    300
+    4    330
+    3    360/0
+
+
+*/
+node2 =new Node(200,200,120,6) // the circle
+one =new line(200,200,250,200)
+Minutes =new line(200,200,200,150)
+Hours=new line(200,200,250,200)
+var currentdate=null
+var timeList=null
 var count=0
 angle=18
 flag=1
-var startTime, endTime;
+flag_on_Changing_Hour=0
 
+var startTime, endTime;
+count=0
+function setTime(){
+    currentdate = new Date(); 
+     var datetime =   currentdate.getHours() + " "  
+                + currentdate.getMinutes() + " " 
+                + currentdate.getSeconds();
+        timeList=datetime.split(" ")
+        timeList[2]=parseInt(timeList[2])
+        timeList[1]=parseInt(timeList[1])
+        timeList[0]=parseInt(timeList[0])
+        temp=12
+        timeList[0]=12
+        timeList[1]=0
+       
+        x = 195 + 70 * Math.cos(-Hour_Minute(timeList[0],timeList[1]))   ///  -convert(180)/2
+        y = 206 + 70 * Math.sin(-Hour_Minute(timeList[0],timeList[1]))   ///
+        Hours.Sx=x
+        Hours.Sy=y
+        Hours.ex=195
+        Hours.ey=206
+
+        x = 195 + 70 * Math.cos(-Hour_Minute(timeList[0],timeList[1]))   ///  -convert(180)/2
+        y = 206 + 70 * Math.sin(-Hour_Minute(timeList[0],timeList[1]))   ///
+
+
+        
+
+       
+            console.log(x,y)
+        
+
+}
+setTime() /// setTime on load
+
+function convert_Hours_to_dgree(Hour){
+    var diff=0;
+    /*if(Hour>3){
+        diff=Hour-3
+        Hour=diff*30
+        return Hour
+    }else{
+        diff=3-Hour
+        Hour=diff*30
+        return Hour
+    }*/
+    diff=3-Hour
+    Hour=diff*30
+    return Hour
+}
+function Minute_(Minute){
+
+}
+function Hour_Minute(Hour,Minute){
+    if(Minute==60){
+        Hour++
+        flag_on_Changing_Hour=1
+        Minute=0
+
+        
+    }else{
+        Minute=(convert(Minute)/2)
+    }
+    Hour=convert(convert_Hours_to_dgree(Hour))
+    //console.log(Hour+Minute)    
+   
+        
+        return  Hour+Minute
+    
+   
+   
+}
+
+function convert(number){   /// will this to convert form Radian to dgree obviously :)
+    number=number/57.295779513
+    return number
+}
 function start() {
   startTime = new Date();
 };
@@ -18,36 +118,41 @@ function end() {
  timeDiff /= 1000;
 
   // get seconds 
-  var seconds = Math.round(timeDiff);
-  console.log(seconds + " seconds");
+  var seconds = timeDiff//Math.round(timeDiff);
+  //console.log(seconds + " seconds");
 return seconds
 }
 
-function eq(cx=200,cy=200,r=60){
+function eq(cx=200,cy=200,r=100){
     if(false){
        // angle=0
-       
+        
     }else{
         angle=angle+0.0025  
-        requestAnimationFrame(Frame)
+       // requestAnimationFrame(Frame)
        
     }
    
-   
-     if(end()==2){
-        x = cx + r * Math.cos(angle)
-        y = cy + r * Math.sin(angle)
-        one.Sx=x
-        one.Sy=y
-        one.ex=200
-        one.ey=200 
+    
+     if(true){
+       /* x = cx + r * Math.cos(-Hour_Minute(8,60))   ///  -convert(180)/2
+        y = cy + r * Math.sin(-Hour_Minute(8,60))   ///
+        
+         
+
+    
+        Minutes.Sx=x
+        Minutes.Sy=y
+        Minutes.ex=200
+        Minutes.ey=200
         flag=1
+        count++*/
+       
          
      }
          
-        
+       
    
-    console.log(angle)
 }
 
 
@@ -59,10 +164,12 @@ function Frame(){
          
     eq()
                       
-    text.clearRect(0,0,window.innerWidth,window.innerHeight);
+   // text.clearRect(0,0,window.innerWidth,window.innerHeight);
     
     node2.draw2()
-    one.draw()
+    //one.draw()
+        //Minutes.draw()
+    Hours.draw()
    
     
        
@@ -72,5 +179,11 @@ function Frame(){
 
 
 
-
 Frame()
+
+/*
+Last Sync: 20/1/2019 @ 20:20:33
+Last Sync: 20/ @ 20:27:44
+Last Sync: 20 28 44
+            H  M   S
+*/
--- code2.js
+++ code2.js
@@ -13,6 +13,7 @@ var sy=0;
 var ex=0;
 var ey=0;
 var flag=1;
+
 function line(sx,sy,ex,ey){ //constructor of the line
 	this.Sx=sx;//// postion of the start line
 	this.Sy=sy;/////////////////////////////
@@ -49,35 +50,71 @@ function Node(x,y,rad,val){  ///// constructor of the Node
 
 	this.string=this.value+"";
 	this.draw2=function(){
-		if(this.nextL!=null&&this.one!=null&&this.leftx!=null){ //// check if ther child (one==true==insert) or dosn't have any left or right child(lefr==flase)
-		this.one.draw();
 	
-	}
-
-	if(this.nextR!=null&&this.one1!=null&&this.rightx!=null){
-		this.one1.draw();
-
-	}
 		text.beginPath();
 		
 		text.arc(this.x,this.y,this.Radius,0,Math.PI*2);
         text.fillStyle="blue";
 		text.stroke();
 		text.font = "10px Arial";
-       text.fillText(this.string,this.x-5,this.y+5);
+       text.fillText("",200,200);
 
 	}
-	this.insert=function(leftx,lefty,rightx,righty){ /// insert node to the left or right
-    this.leftx=leftx;  ///
-    this.lefty=lefty;
-    this.rightx=rightx;
-    this.righty=righty;
-    
-    this.one=new line(this.x+18	,this.y+10,this.leftx+5,this.lefty-15);// construct a line bettween the child Node and the father Node
-
-	this.one1=new line(this.x,this.y+10,this.rightx-5,this.righty-15);
-	}/// 
+	
+	for(var i=1;i<=12;i++){
+		/*if(i<3){
+			x = 200 + (rad-12)* Math.cos(Hour_Minute(i,0))   ///  -convert(180)/2
+			y = 200 + 100* Math.sin(Hour_Minute(i,0))
+		}else{
+			x = 200 + 105* Math.cos(-Hour_Minute(i,0))   ///  -convert(180)/2
+			y = 200 + 105* Math.sin(-Hour_Minute(i,0))   ///
+		}
+		if(i==12){
+			x = 200 + 105* Math.cos(-Hour_Minute(i,0))   ///  -convert(180)/2
+			y = 200 + (rad-12)* Math.sin(-Hour_Minute(i,0))   ///
+		}
+		if(i==11||i==3||i==4){
+			x = 200 + 103* Math.cos(-Hour_Minute(i,0))   ///  -convert(180)/2
+			y = 200 + (rad-12)* Math.sin(-Hour_Minute(i,0))   ///
+		}*/
+		
+			x = 195 + 100* Math.cos(-Hour_Minute(i,0))   ///  -convert(180)/2
+			y = 206 + 100* Math.sin(-Hour_Minute(i,0))   ///
+		
+			text.font = "19px Arial";
+		if(i==3	){
+			text.fillText(i,x,y+5);
+		
+		}else if(i==4){
+			text.fillText(i,x,y+7);
+		}else if(i==5){
+			text.fillText(i,x-7,y);
+		}else if(i==6){
+			text.fillText(i,x-5,y);
+		}
+		//////
+		else if(i==9){
+			text.fillText(i,x,y+5);
+		}else if(i==10){
+			text.fillText(i,x-3,y+5);
+		}else if(i==11){
+			text.fillText(i,x-5,y+5);
+		}else if(i==12){
+			text.fillText(i,x-8,y);
+		}
+		
+		else{
+			text.fillText(i,x,y);
+		}
+			
+		
+		
+		
+		
+		
+	}
 	
 
 
 }
+
